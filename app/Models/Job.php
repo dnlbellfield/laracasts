72394c31a009d15f836c799a408063c93e3e653d
Daniel Bellfield <dnlbellfield@gmail.com>
2024-04-08 11:59:36 -0700

    update

---
+++ app/Models/Job.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Job extends Model {
+
+  use HasFactory;
+
+  protected $table = 'job_listings';
+
+  protected $fillable = ['title', 'salary'];
+
+  public function employer() 
+  {
+      return $this->BelongsTo(Employer::class);
+  }
+
+  public function tags() 
+  {
+      return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+  }
+}
